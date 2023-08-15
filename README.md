ef37961ac4a6d1c54e4dc6d845239132eddf8351
Domenic <zhangdomenic@outlook.com>
2023-08-15 20:59:07 +0800

    commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# glados自动签到，实现无限白嫖。
+
+原仓库地址：https://github.com/lukesyy/glados_automation
+
+复制了一个仓库，防止原仓库被 Github 封后，Fork 也都失效
+
+环境变量：`GLADOS_COOKIE`（必要） 和 `PUSHPLUS_TOKEN`（非必要）
+
+ `GLADOS_COOKIE`多个账号需使用 '&' 隔开
+  示例：cookie&cookie&cookie
+
+
+
+# 食用方法 （github_actions）
+
+ 1.点击右上角 **fork** 按钮 
+ 
+ 2.在自己仓库中打开此项目
+ 
+ 3.配置环境变量
+ 
+ 4.点亮右上角的星星 **star** 激活 actions
+ 
+ 5.然后点击 Actions 标签查看运行的详细状况
+ 
+ # 青龙面板
+   直接把 glados_Qinglong.py 文件放到青龙里 环境变量同上。
+   
+ # 自动同步上游代码（非必要）
+  环境变量名称 `PAT`
+ 点击 https://github.com/settings/tokens 按下图操作 申请一个秘钥 放到环境变量里就可以了
+ 
+ ![微信截图_20230423132436](https://user-images.githubusercontent.com/70319988/233821350-908a0752-658d-4452-919d-8e7b1acf81f4.png)
+![微信截图_20230423132355](https://user-images.githubusercontent.com/70319988/233821351-f7644740-325b-41cb-8d86-0b9692d9eedb.png)
+ 
+ # Tips
+ 
+   当你签到差不多一个月的时候就不能百分百获得一天的使用额度，而是有几率获得一天的额度。如果觉得好用建议 购买一个30天的pro套餐 就可以一直签到一直续杯了。
+
+![image](https://user-images.githubusercontent.com/70319988/231369203-c812910a-963d-45b8-98a5-95b2623c25d7.png)
+![image](https://user-images.githubusercontent.com/70319988/199923789-639e8295-b03e-4abd-858e-ff427015512a.png)
+![image](https://user-images.githubusercontent.com/70319988/199923884-d81dd457-ecc5-4de9-b480-191d25217c47.png)
+
---
+++ glados.py
@@ -0,0 +1,42 @@
+import requests,json,os
+# -------------------------------------------------------------------------------------------
+# github workflows
+# -------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+# pushplus秘钥 申请地址 http://www.pushplus.plus
+    sckey = os.environ.get("PUSHPLUS_TOKEN", "")
+# 推送内容
+    sendContent = ''
+# glados账号cookie 直接使用数组 如果使用环境变量需要字符串分割一下
+    cookies = os.environ.get("GLADOS_COOKIE", []).split("&")
+    if cookies[0] == "":
+        print('未获取到COOKIE变量') 
+        cookies = []
+        exit(0)
+    url= "https://glados.rocks/api/user/checkin"
+    url2= "https://glados.rocks/api/user/status"
+    referer = 'https://glados.rocks/console/checkin'
+    origin = "https://glados.rocks"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+    payload={
+        'token': 'glados.one'
+    }
+    for cookie in cookies:
+        checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+        state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+    #--------------------------------------------------------------------------------------------------------#  
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
+        email = state.json()['data']['email']
+        if 'message' in checkin.text:
+            mess = checkin.json()['message']
+            print(email+'----结果--'+mess+'----剩余('+time+')天')  # 日志输出
+            sendContent += email+'----'+mess+'----剩余('+time+')天\n'
+        else:
+            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
+            print('cookie已失效')  # 日志输出
+     #--------------------------------------------------------------------------------------------------------#   
+    if sckey != "":
+         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
+
+
